70cd4dabf81232e6eda173cb946a003a284a4727
Alao Emmanuel <emmanuelalao0'gmail.com>
2023-10-06 05:01:42 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: A printf function that prints
+ *with proper grammer, but the outcome is a piece of art,
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the\n");
+	return (0);
+}
