f861f6adc67639c9eb48d2c10b96f2b284659126
daremmy <emmanuelalao0@gmail.com>
2024-07-02 09:19:24 -0700

    Did task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * _puts - prints a string, followed by a new line
  * @str - the string to be printed
@@ -7,5 +9,12 @@
 
 void _puts(char *str)
 {
-	printf("%s\n",str);
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
