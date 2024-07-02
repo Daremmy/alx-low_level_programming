67d23cc21d4f9943a429ccf36700006addd61f91
daremmy <emmanuelalao0@gmail.com>
2024-07-02 12:19:52 -0700

    Did task 5

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,22 +8,15 @@
  */
 
 void print_rev(char *s)
-{
-	int a,b,c;
-	char d;
+{	int i, n;
 
-	a = 0;
+	n = 0;
+	while (s[n] != '\0')
+		n++;
 
-	while (s[a] != '\0')
+	for (i = n - 1; i >= 0; i--)
 	{
-		a++
+		_putchar(s[i]);
 	}
-
-	c = a - 1;
-	for (b = 0; c >= 0 && b < c; c--, b++)
-	{
-		d = s[b];
-		s[b] = s[c];
-		s[c] = d;
-	}
+	_putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: the string to be reversed
+ *
+ * Return: The reverse of the string
+ */
+
+void print_rev(char *s)
+{
+	int a,b,c;
+	char d;
+
+	a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++
+	}
+
+	c = a - 1;
+	for (b = 0; c >= 0 && b < c; c--, b++)
+	{
+		d = s[b];
+		s[b] = s[c];
+		s[c] = d;
+	}
+}
