164538091f65393296fcbf04914901a3fd812c02
daremmy <emmanuelalao0@gmail.com>
2024-07-02 00:33:58 -0700

    Did task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * 0-reset_to_98.c - Updates an int value using its pointer
+ * @*n: The pointer used to update the int value
+ *
+ * Changes the value of an int stored in an adress to 98 from the pointer.
+ * Return: void
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
