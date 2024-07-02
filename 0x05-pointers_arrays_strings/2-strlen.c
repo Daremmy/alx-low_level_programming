37f6bd34508404ebab288c95f21c6948fbd998c3
daremmy <emmanuelalao0@gmail.com>
2024-07-02 05:54:41 -0700

    Did task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen - returns the lenght of a string
+ * @s: variable whose string lenght would be counted
+ *
+ *Return: String lenght
+ */
+
+int _strlen(char *s)
+	int l = 0;
+
+	while (s[l] ! = '\0')
+	{
+		l++;
+	}
+	return (l);
