917e5a044ea5c0d72542876991b8dad6ef902043
daremmy <emmanuelalao0@gmail.com>
2024-07-02 18:22:00 -0700

    Did task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+/**
+ * _atoi - converts strings to integers
+ * @s: string to be converted
+ *
+ * Return: Interger
+ */
+
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int sign = 1;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+
+	return (num * sign);
+}
