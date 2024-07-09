5dabf2a9f4e6f864d409eb073b12846cdd8d163c
daremmy <emmanuelalao0@gmail.com>
2024-07-08 23:15:37 -0700

    Did task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,14 +6,14 @@
  * @b: constant byte to fill the memory with
  * @n: number of bytes to fill
  *
- * Return: s
+ * Return: pointer to memory area of s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n, ++i)
+	for (i = 0; i < n; ++i)
 	{
 		s[i] = b;
 	}
