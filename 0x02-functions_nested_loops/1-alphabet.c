bcb7130947f8af26b1e04828b8b25cf16d566da1
daremmy <emmanuelalao0@gmail.com>
2024-08-11 10:38:16 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase followed by a new line
+ *
+ * Return: return 0
+ */
+
+void print_alphabet(void)
+{
+	char lalp;
+	
+	for (lalp = 'a'; lalp <= 'z'; lalp++)
+		_putchar(lalp\n);
+}
