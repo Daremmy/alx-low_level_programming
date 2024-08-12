a6d135f2adb41efde5d029cca64199d13f488d3d
daremmy <emmanuelalao0@gmail.com>
2024-08-11 19:02:33 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 lines of the alphabet in lower case
+ *
+ * Return: return 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char lalp;
+	int l;
+
+	for (l = 1; l <=10; l++)
+	{
+		for (lalp = 'a'; lalp <= 'z' lalp++)
+			_putchar(lalp);
+		_putchar('\n');
+	}
+}
